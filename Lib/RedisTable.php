fe10f829404599a7bc2d0fbb8fcb1abdb0dd3208
stealhead <418364620@qq.com>
2017-02-17 11:00:54 +0800

    modified:   RedisTable.php

--- Lib/RedisTable.php
+++ Lib/RedisTable.php
@@ -3,8 +3,10 @@ namespace Lib;
 class RedisTable extends \Lib\GRedis{
     public function __construct($config = null)
     {
-        $confObj = new \Lib\Config();
-        $config = $confObj->getConf('redis');
+        if(!$config){
+            $confObj = new \Lib\Config();
+            $config = $confObj->getConf('redis');
+        }
         parent::__construct();
         $this->setConfig($config);
         return $this->connect();
